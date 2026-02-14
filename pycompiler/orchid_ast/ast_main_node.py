02526a33abaa11927fa34dc88d7f7e1dc2723af3
Riley <77288737+riley0122@users.noreply.github.com>
2026-02-14 13:36:23 +0100

    Make main into token

--- pycompiler/orchid_ast/ast_main_node.py
+++ pycompiler/orchid_ast/ast_main_node.py
@@ -1,6 +1,6 @@
 from orchid_ast.ast_function_node import Ast_function_node
-from orchid_ast.ast_type import Ast_type
+from lexer.token import Token
 
 class Ast_main_node(Ast_function_node):
     def __init__(self, body):
-        super().__init__("main", [], [], body)
+        super().__init__(Token("main"), [], [], body)
